9649cbc6e6cc6500bc9a0c8583135819ae0bed48
WalbertJr <54550066+WalbertJr@users.noreply.github.com>
2020-10-14 09:12:16 -0600

    Begin Transaccion Al momento de Guardar los datos.

--- Prueba_Middleware/ApiMiddleware/Controllers/DepartamentosController.cs
+++ Prueba_Middleware/ApiMiddleware/Controllers/DepartamentosController.cs
@@ -44,30 +44,37 @@ namespace ApiMiddleware.Controllers
 
             var departamentoinDb = new DEPARTAMENTOS() { REFERENCIA = departamento.REFERENCIA, DESCRIPCION = departamento.DESCRIPCION, NOMBRE = departamento.NOMBRE };
 
-            try
+            using (var _db = db.Database.BeginTransaction())
             {
-                db.DEPARTAMENTOS.Add(departamentoinDb);
-                db.SaveChanges();
-
-                if (departamento != null)
+                try
                 {
-                    if (departamento.EMPLEADOS.Count() > 0)
+                    db.DEPARTAMENTOS.Add(departamentoinDb);
+                    db.SaveChanges();
+
+                    if (departamento != null)
                     {
-                        foreach (var item in departamento.EMPLEADOS)
+                        if (departamento.EMPLEADOS.Count() > 0)
                         {
-                            db.EMPLEADOS.Add(new EMPLEADOS() { DESCRIPCION = item.DESCRIPCION, FECHA_NACIMIENTO = item.FECHA_NACIMIENTO, GENERO_SEXO = item.GENERO_SEXO, ID_DEPARTAMENTO = departamentoinDb.ID_DEPARTAMENTO, NOMBRE = item.NOMBRE });
-                            db.SaveChanges();
+                            foreach (var item in departamento.EMPLEADOS)
+                            {
+                                db.EMPLEADOS.Add(new EMPLEADOS() { DESCRIPCION = item.DESCRIPCION, FECHA_NACIMIENTO = item.FECHA_NACIMIENTO, GENERO_SEXO = item.GENERO_SEXO, ID_DEPARTAMENTO = departamentoinDb.ID_DEPARTAMENTO, NOMBRE = item.NOMBRE });
+                                db.SaveChanges();
+                            }
                         }
                     }
-                }
 
-                response.Message = "Departamento almacenado correctamente";
-                response.Result = true;
+                    _db.Commit();
+
+                    response.Message = "Departamento almacenado correctamente";
+                    response.Result = true;
+                }
+                catch (Exception E)
+                {
+                    _db.Rollback();
+                    response.Message = "Departamento no almacenado correctamente, debe ingresar todos los datos de los empleados";
+                }
             }
-            catch (Exception E)
-            {
-                response.Message = "Departamento no almacenado correctamente, debe ingresar todos los datos de los empleados";
-            }
+
             return response;
         }
 
